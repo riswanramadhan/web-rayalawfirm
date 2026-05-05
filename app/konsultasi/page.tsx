06f6ce02a0ad145dd08f0af1ed985d9906000fbb
AndiRoihan <roihanaqilah72@gmail.com>
2026-05-06 02:41:41 +0800

    style: change konsultasi page background to navy
    8. ganti navbar konsultasi ke navy

--- app/konsultasi/page.tsx
+++ app/konsultasi/page.tsx
@@ -44,8 +44,8 @@ const steps = [
 
 export default function KonsultasiPage() {
   return (
-    <main className="bg-offwhite">
-      <section className="pt-24 pb-16 lg:pt-28 lg:pb-20">
+    <main className="bg-navy">
+      <section className="pt-24 pb-16 lg:pt-20 lg:pb-20">
         <div className="grid lg:grid-cols-[1fr_1.25fr]">
           <div className="relative overflow-hidden bg-navy px-6 py-16 lg:px-16">
             <div className="absolute -right-20 top-10 h-48 w-48 rounded-full border border-primary/10" />
